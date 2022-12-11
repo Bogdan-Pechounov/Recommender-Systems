3feecb4077e745be43b7dc66f60a625e66afea27
Bogdan-Pechounov <119967588+Bogdan-Pechounov@users.noreply.github.com>
2022-12-11 02:54:06 -0500

    Update app.js

--- api/app.js
+++ api/app.js
@@ -5,13 +5,12 @@ const cors = require('cors')
 //Start server and database connection
 const app = express()
 
-const PORT = 3001
-const DB_URI = 'mongodb://127.0.0.1:27017/movies'
+const PORT = process.env.PORT || 3001
+const DB_URI = process.env.DB_URI || 'mongodb://127.0.0.1:27017/movies'
+const origin = process.env.ORIGIN || 'http://localhost:3000'
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`)
-  console.log(process.env.RENDER)
-  console.log(process.env)
 })
 app.get('/', (_, res) => res.send('Hello there!'))
 
@@ -21,15 +20,17 @@ mongoose
     console.log('Connected to database:', DB_URI)
   })
   .catch((err) => {
-    console.log("Can't connect to database")
+    console.log("Can't connect to database:", DB_URI)
     console.log(err)
   })
 
 //Middleware
-//TODO exclude POST PUT in production
 app.use(express.json())
-app.use(cors({ origin: 'http://localhost:3000' }))
+app.use(cors({ origin }))
 
 //Routes
 app.use('/movies', require('./routes/movies'))
 app.use('/ratings', require('./routes/ratings'))
+app.get('/test/:id', (req, res) => {
+  res.send(req.params)
+})
